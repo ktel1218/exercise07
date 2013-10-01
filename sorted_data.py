bca1b6d0b860aa50443446444beb588d45946386
Hackbright Student <user@mini-005.(none)>
2013-09-30 17:21:50 -0700

    short code

--- sorted_data.py
+++ sorted_data.py
@@ -1,4 +1,3 @@
-
 '''
 Your job is to write a program named 'sorted_data.py' reads the file, then spits out the ratings in alphabetical order by restaurant
 '''
@@ -16,27 +15,19 @@ def main():
     dict_of_restaurants = {}
     dict_of_ratings = {}
     list_of_restaurants = open_and_read(test_file).split("\n")
+    
     for i in list_of_restaurants:
         i = i.split(":")
-        if i[1] in dict_of_ratings:
-            dict_of_ratings[i[1]].append(i[0])
-        else:
-            dict_of_ratings[i[1]] = []
-            dict_of_ratings[i[1]].append(i[0])
-
-        dict_of_restaurants[i[0]] = int(i[1])
-        #dict_of_ratings[i[1]] = dict_of_ratings.get(i[1], []).append(dict_of_ratings[i[0]])
-
         #list by rating, highest to lowest (build a dictionary)
+        dict_of_restaurants[i[0]] = int(i[1])
+        dict_of_ratings.setdefault(i[1], []).append(i[0])
 
-    print "List of restaurants: ", list_of_restaurants
+    sorted_restaurants = sorted(dict_of_restaurants.iteritems())
+    for i in range(len(sorted_restaurants)):
+        print "%s has %d stars!" % (sorted_restaurants[i][0], sorted_restaurants[i][1])
 
-    sorted_ratings = sorted(dict_of_restaurants.iteritems())
+    sorted_ratings = sorted(dict_of_ratings.iteritems(), reverse = True)
     for i in range(len(sorted_ratings)):
-        print "%s has %d stars!" % (sorted_ratings[i][0], sorted_ratings[i][1])
-
-    print sorted(dict_of_ratings.iteritems(), reverse = True)
-
-
+        print sorted_ratings[i][0],"stars: ", sorted_ratings[i][1]
 
 main()
